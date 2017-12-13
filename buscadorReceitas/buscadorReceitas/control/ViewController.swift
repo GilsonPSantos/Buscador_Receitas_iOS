9fcdd9db433e955d3846d148b04307b335efc354
gilsonsantos <mac@macs-MacBook-Pro.local>
2017-12-13 12:31:27 -0200

    Primeiro comit

---
+++ buscadorReceitas/buscadorReceitas/control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buscadorReceitas
+//
+//  Created by mac on 13/12/2017.
+//  Copyright © 2017 buscadorReceitas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
